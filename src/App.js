1bacd661fe8db61c67d8368cfa6dd3f810dc035c
nuelKaiser <majekodunmiimmanuel9@gmail.com>
2022-10-12 02:43:23 +0100

    removed unused dependencies

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { useDataLayerValue } from './DataLayer';
 const spotify = new SpotifyWebApi();
 
 function App() {
-  
+
   const [{ user, token }, dispatch] = useDataLayerValue();
 
   useEffect(() => {
@@ -17,7 +17,7 @@ function App() {
     window.location.hash = '';
     const _token = hash.access_token
 
-    if(_token) {
+    if (_token) {
       dispatch({
         type: 'SET_TOKEN',
         token: _token
@@ -30,38 +30,38 @@ function App() {
           type: 'SET_USER',
           user: user
         })
-        
+
       })
     }
-   
-    spotify.getUserPlaylists().then( (playlists) => {
+
+    spotify.getUserPlaylists().then((playlists) => {
       dispatch({
         type: 'SET_PLAYLISTS',
         playlists: playlists
       })
-    } )
+    })
 
     spotify.getPlaylist('37i9dQZEVXcEx4auSetwFz').then(
       (response) => {
-          dispatch({
-            type: 'SET_DISCOVER_WEEKLY',
-            discover_weekly: response,
-          })
-          console.log(response)
+        dispatch({
+          type: 'SET_DISCOVER_WEEKLY',
+          discover_weekly: response,
+        })
+        console.log(response)
       }
 
     )
 
-    
+
   }, [dispatch, token, user])
 
   return (
     <div className="App">
-     {
-      token ?
-       <Player spotify={spotify} /> : 
-      <Login />
-     }
+      {
+        token ?
+          <Player spotify={spotify} /> :
+          <Login />
+      }
     </div>
   );
 }
